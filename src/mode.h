b80294e7d8f4916822603ccfdf42681c61cbfe11
CMSC 355 <99101935+ERB355@users.noreply.github.com>
2022-11-10 02:46:05 +0000

    Update mode.h

--- src/mode.h
+++ src/mode.h
@@ -92,7 +92,7 @@
 //----------------------------------- Code Starts Here ----------------------------------
 /* GOAL: Define the objcolor behavior to declare it as a tool in  xFig  and assign it the
  * the number 63 as the tool number. You will use it in the file 'w_modepanel.c'.      */
-
+#define     F_OBJCOLOR       63
 //----------------------------------- Code ends Here ------------------------------------
 
 extern int	cur_mode;
